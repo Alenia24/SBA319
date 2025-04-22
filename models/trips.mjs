293edfba94402e163694e80adb510a06351acb13
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-21 20:50:55 -0400

    Update trip schema

--- models/trips.mjs
+++ models/trips.mjs
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
-const tripSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  destination: { type: String, required: true },
-  start: { type: String, required: true },
-  end: { type: String, required: true },
-  price: { type: Number, required: true },
-});
+const tripSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    destination: { type: String, required: true },
+    start: { type: String, required: true },
+    end: { type: String, required: true },
+    price: { type: Number, required: true },
+},{ versionKey: false });
 
 const Trip = mongoose.model("Trip", tripSchema);
 
