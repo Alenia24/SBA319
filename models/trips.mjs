5daf4b8a49632fac5199c15d84202a31285ea763
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-21 20:43:30 -0400

    Create trip schema

---
+++ models/trips.mjs
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const tripsSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  destination: { type: String, required: true },
+  start: { type: String, required: true },
+  end: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const Trip = mongoose.model("Trip", tripsSchema);
+
+export default Trip;
