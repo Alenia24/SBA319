4dd8a2142e15fb05d53144f3b849463a296f754d
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-21 21:20:58 -0400

    Set up get all trips route

--- routes/trips.mjs
+++ routes/trips.mjs
@@ -1,10 +1,18 @@
 import express from "express";
-import Trip from "../models/trip.mjs"
+import Trip from "../models/trip.mjs";
 
 const router = express.Router();
 
+// Get all the trips
 router.get("/", async (req, res) => {
-    res.send("Welcome to trip route!")
-})
+  try {
+    const trips = await Trip.find();
+    res.json(trips);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
 
 export default router;
