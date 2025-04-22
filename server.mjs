9cda71be7a54ecdc1a8292ea61f3ac82ff6a61e1
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-21 20:33:06 -0400

    Connect mongoose to mongoDB

--- server.mjs
+++ server.mjs
@@ -1,9 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Mongoose Connection
+mongoose.connect(process.env.ATLAS_URI);
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoDB");
+});
 
 app.get("/", (req, res) => {
     res.send("Welcome to the API.")
