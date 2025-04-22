572355d48bbfafa7c930a84a1a99ac98ca1b58f1
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-21 22:01:18 -0400

    Set up get all itenaries route

--- routes/itenaries.mjs
+++ routes/itenaries.mjs
@@ -3,8 +3,14 @@ import Itenary from "../models/itenary.mjs";
 
 const router = express.Router();
 
+// Get all the trips
 router.get("/", async (req, res) => {
-    res.send("Welcome to itenaries route!")
-})
+  try {
+    const itenaries = await Itenary.find();
+    res.json(itenaries);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default router;
