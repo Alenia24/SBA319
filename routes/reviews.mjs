4ac8e2487479578fa95d57bf9d51b3d4f141faeb
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-23 11:02:05 -0400

    Create review route and add get all reviews

---
+++ routes/reviews.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import Review from "../models/review.mjs";
+
+const router = express.Router();
+
+// Get all the reviews
+router.get("/", async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.json(reviews);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export default router;
